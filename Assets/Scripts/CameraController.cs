535b340aab0201f6bcfe2246f6157830b317d1fa
RulerAlva <siebeboogaerts@gmail.com>
2025-04-28 15:23:59 +0200

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,6 +14,7 @@ public class CameraController : MonoBehaviour
     private Camera _followerCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //test
     void Start()
     {
         _mainCamera.transform.position = _followerCamera.transform.position;
