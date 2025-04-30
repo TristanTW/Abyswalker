f2fb855743077b0250ff8f0887fb4bdf3acb36ef
DumsyI <dumsyman@gmail.com>
2025-04-30 17:34:37 +0200

    Fixed Wall collision
    We went trough the walls
    The problem was with rigidbody and the use of "Transform" to move around

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,41 +4,30 @@ public class CharacterControll : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody _rb;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         int forward = 0;
         int backward = 0;
         int left = 0;
         int right = 0;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            forward = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            backward = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            left = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            right = 1;
-        }
+        if (Input.GetKey(KeyCode.W)) forward = 1;
+        if (Input.GetKey(KeyCode.S)) backward = -1;
+        if (Input.GetKey(KeyCode.A)) left = -1;
+        if (Input.GetKey(KeyCode.D)) right = 1;
 
         Vector3 directionVector = new Vector3(left + right, 0, forward + backward);
+        Vector3 movementVector = directionVector.normalized * _speed * Time.fixedDeltaTime;
 
-        Vector3 movementVector = directionVector.normalized;
-
-        transform.position = new Vector3(transform.position.x + (movementVector.x * _speed * Time.deltaTime), transform.position.y, transform.position.z + (movementVector.z * _speed * Time.deltaTime));
+        _rb.MovePosition(_rb.position + movementVector);
     }
 }
+
