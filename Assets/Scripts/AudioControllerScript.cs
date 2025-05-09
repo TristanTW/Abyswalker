8201163419e3ce0c0fedffa7435c1f7076d35425
RulerAlva <siebeboogaerts@gmail.com>
2025-05-09 14:17:06 +0200

    Audio (still needs implementation)
    ik moet nog op de plekken zeggen (play)
    wanneer de enemy damge doet/neemt gewoon currentclip = damage en audiosource.play

---
+++ Assets/Scripts/AudioControllerScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioControllerScript : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _takeDamage, _doDamage, _collectCoin;
+    public AudioClip currentclip = null;   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _audioSource.clip = currentclip;
+        _audioSource.Play();
+    }
+}
